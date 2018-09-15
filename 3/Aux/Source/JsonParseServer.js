26c6e281ef095512abcdf58ee58d88a3a5784807
Suzanne Becker <suzanne@ccs.neu.edu>
2018-09-15 18:02:41 -0400

    Create server that parses JSON input over a TCP connection

---
+++ 3/Aux/Source/JsonParseServer.js
@@ -0,0 +1,23 @@
+const net = require('net');
+const JsonParse = require('./lib/JsonParse.js');
+const { StringDecoder } = require('string_decoder');
+
+
+const server = net.createServer({ "allowHalfOpen":true }, function(socket) {
+  const decoder = new StringDecoder('utf8');
+  let buffer = "";
+  socket.on('data', function(chunk) {
+    let decoded = decoder.write(chunk);
+    buffer += decoded;
+    //console.log("Received: " + decoded);
+  });
+
+  socket.on('end', function() {
+    let data = JsonParse.parseInputString(buffer);
+    //console.log("Final data sent back:  " + data);
+    socket.end(data);
+    buffer = "";
+  });
+});
+
+server.listen(8000, '127.0.0.1');
