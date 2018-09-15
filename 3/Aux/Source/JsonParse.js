e312bf317c4b26b31747d5505d20b58778179085
Zach Walsh <zcwalsh@ccs.neu.edu>
2018-09-14 23:21:38 -0400

    Use the nearley package to parse JSON, abstract the parser over the input

---
+++ 3/Aux/Source/JsonParse.js
@@ -0,0 +1,17 @@
+const nearley = require("nearley");
+const jsonGrammar = require("./JsonGrammar.js");
+const jsonParser = new nearley.Parser(nearley.Grammar.fromCompiled(jsonGrammar));
+
+function jsonArrayToString(arr) {
+	let outputString = '';
+	for (let index = 0; index < arr.length; index++) {
+		let outputObject = '[' + (arr.length - index - 1) + ',' + JSON.stringify(arr[index]) + ']\n';
+		outputString += outputObject;
+	}
+	return outputString;
+}
+
+exports.parseInputString = function(inputString) {
+	jsonParser.feed(inputString);
+	return(jsonArrayToString(jsonParser.results[0]));
+}
---
+++ 3/Aux/Source/JsonParseStdIn.js
@@ -0,0 +1,15 @@
+const JsonParse = require('./JsonParse.js')
+let input = '';
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+	let chunk = process.stdin.read();
+	if (chunk != null) {
+		input += chunk;
+	}
+});
+
+process.stdin.on('end', () => {
+	console.log(JsonParse.parseInputString(input));
+});
