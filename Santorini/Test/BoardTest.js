fbe0bc189355ff2bd5c070f81563cd67d305fb44
Zach Walsh <zcwalsh@ccs.neu.edu>
2018-10-05 12:13:47 -0700

    Build Board unit test skeleton

--- Santorini/Test/BoardTest.js
+++ Santorini/Test/BoardTest.js
@@ -1,1 +1,63 @@
-//
+describe("add worker", function() {
+  it("adds a worker to the board", function() {
+
+  });
+  it("adds four workers to the board", function() {
+
+  });
+  it("does not allow more than four workers to be added", function() {
+
+  });
+  it("does not allow workers to be placed on top of other workers", function() {
+
+  });
+});
+
+describe("move worker", function() {
+  it("checks that the worker exists", function() {
+
+  });
+  it("checks that destination is on the board and empty", function() {
+
+  });
+  it("moves the worker to that location", function() {
+
+  });
+  it("does not move any other workers", function() {
+
+  });
+});
+
+describe("build floor", function() {
+  it("checks that the worker exists", function() {
+
+  });
+  it("checks that destination is on the board and empty", function() {
+
+  });
+  it("checks that the target height is less than max height", function() {
+
+  });
+  it("increases the height of that cell", function() {
+
+  });
+});
+
+describe("getters", function() {
+  describe("can't mutate internal state", function() {
+    it("getHeights returns a copy", function() {
+
+    });
+    it("getWorkers returns a copy", function() {
+
+    });
+    it("getWorker returns a copy", function() {
+
+    });
+  });
+  describe("returns correct values", function() {
+    it("getHeight checks that the value is on the board", function() {
+
+    });
+  })
+});
