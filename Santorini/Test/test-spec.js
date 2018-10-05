a5b18e98dba8467339ded2135a05f42250df1304
Suzanne Becker <suzanne@ccs.neu.edu>
2018-10-05 14:30:13 -0400

    jasmine  config file, sample test, and script to run all tests in project

---
+++ Santorini/Test/test-spec.js
@@ -0,0 +1,9 @@
+describe("please work", function() {
+    it("hopefully works", function () {
+        expect("lol").toBe("lol");
+    });
+
+    it("hopefully fails", function () {
+        expect("lol").toBe("fuck");
+    });
+});
