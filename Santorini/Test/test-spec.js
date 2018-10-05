e32d097cdda5e4fe35e293d9e1a5415b002e5481
Suzanne Becker <suzanne@ccs.neu.edu>
2018-10-05 18:35:03 -0400

    clean up trial test

--- Santorini/Test/test-spec.js
+++
@@ -1,9 +0,0 @@
-describe("please work", function() {
-    it("hopefully works", function () {
-        expect("lol").toBe("lol");
-    });
-
-    it("hopefully fails", function () {
-        expect("lol").toBe("fuck");
-    });
-});
