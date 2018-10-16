1d48037de65b295ae526cf0738740063adb3b865
Suzanne Becker <suzanne@ccs.neu.edu>
2018-10-15 23:18:30 -0400

    Unit tests for compound strategy

--- Santorini/Test/CompoundStrategyTest.js
+++ Santorini/Test/CompoundStrategyTest.js
@@ -1,8 +1,11 @@
 let expect = require('chai').expect;
+let sinon = require('sinon');
 const Board = require('./../Common/Board.js');
 const GameState = require('./../Common/GameState.js');
 const Action = require('./../Common/Action.js');
 const PlaceAction = Action.PlaceAction;
+const MoveAction = Action.MoveAction;
+const BuildAction = Action.BuildAction;
 const CompoundStrategy = require('../Player/CompoundStrategy.js');
 
 describe("Compound strategy", function () {
@@ -11,17 +14,41 @@ describe("Compound strategy", function () {
     board = new Board();
     gameState = new GameState(board);
   });
-  describe("before all Workers have been placed", function () {
+  describe("when used to produce a worker placement", function () {
+    let placementStrategyMock, chooseTurnStrategyMock,  compoundStrategy,
+        placeAction;
+    beforeEach(function () {
+      placeAction = new PlaceAction([0,0]);
+      placementStrategyMock = sinon.fake.returns(placeAction);
+      chooseTurnStrategyMock = sinon.fake();
+      compoundStrategy = new CompoundStrategy(placementStrategyMock, chooseTurnStrategyMock);
+    });
     it("returns the Action given by the choosePlacement strategy", function () {
-      
+      let givenPlaceAction = compoundStrategy.nextPlacement(gameState);
+      expect(givenPlaceAction).to.eql(placeAction);
+    });
+    it("does not call any methods on the chooseTurn strategy", function() {
+      expect(chooseTurnStrategyMock.callCount).to.eql(0);
     });
   });
-  describe("after all Workers have been placed", function () {
-    it("calls the chooseTurn strategy to get a Turn", function () {
-
+  describe("when used to produce a Turn", function () {
+    let placementStrategyMock, chooseTurnStrategyMock,  compoundStrategy,
+      workerId, moveAction, buildAction;
+    beforeEach(function () {
+      workerId = 0;
+      moveAction = new MoveAction(workerId, [0,0]);
+      buildAction = new BuildAction(workerId, [1,1]);
+      placementStrategyMock = sinon.fake();
+      chooseTurnStrategyMock = sinon.fake.returns([moveAction, buildAction]);
+      compoundStrategy = new CompoundStrategy(placementStrategyMock, chooseTurnStrategyMock);
     });
-    it("uses the Actions of that Turn one-by-one", function () {
-
+    it("returns the Turn given by the chooseTurn strategy", function () {
+      let givenTurn = compoundStrategy.nextTurn(gameState);
+      expect(givenTurn[0]).to.eql(moveAction);
+      expect(givenTurn[1]).to.eql(buildAction);
+    });
+    it("does not call any methods on the choosePlacement strategy", function() {
+      expect(placementStrategyMock.callCount).to.eql(0);
     });
   });
 });
