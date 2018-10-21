09330bb226ed7bb44f2e073b657c0c1d80c935d8
Suzanne Becker <suzanne@ccs.neu.edu>
2018-10-21 14:20:36 -0400

    Add constants for GameState and EndGameReason, used by Referee

--- Santorini/Lib/constants.js
+++ Santorini/Lib/constants.js
@@ -5,5 +5,12 @@ module.exports = Object.freeze({
   BOARD_WIDTH: 6,
   BOARD_HEIGHT: 6,
   MAX_FLOORS_PER_MOVE: 1,
-  WINNING_HEIGHT: 3 // Winning height is 1 less than max height
+  WINNING_HEIGHT: 3, // Winning height is 1 less than max height
+  EndGameReason: {
+    WON: "WON",
+    BROKEN_RULE: "BROKEN_RULE",
+  },
+  GameState: {
+    IN_PROGRESS: "IN_PROGRESS"
+  }
 });
