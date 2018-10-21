cbeeca0342221c661a3a532f76a878531bd87457
Suzanne Becker <suzanne@ccs.neu.edu>
2018-10-21 14:17:33 -0400

    replace direction strings (EAST, etc.) with constants, and use those constants in Direction file. Add method to check if a piece of data is a Direction.

--- Santorini/Lib/direction.js
+++ Santorini/Lib/direction.js
@@ -23,6 +23,18 @@ let directions = {
 
 module.exports.directions = directions;
 
+const EAST = "EAST";
+const WEST = "WEST";
+const NORTH = "NORTH";
+const SOUTH = "SOUTH";
+const PUT = "PUT";
+
+module.exports.EAST = EAST;
+module.exports.WEST = WEST;
+module.exports.NORTH = NORTH;
+module.exports.SOUTH = SOUTH;
+module.exports.PUT = PUT;
+
 // Returns the target tile posn in the given direction of the given x/y
 // Direction Int Int -> Posn
 module.exports.directionToCoordinate = function (direction, x, y) {
@@ -36,20 +48,30 @@ module.exports.coordToDirection = function (posn) {
   let out = [];
 
   if (posn.x < 0) {
-    out.push("WEST");
+    out.push(WEST);
   } else if (posn.x > 0) {
-    out.push("EAST");
+    out.push(EAST);
   } else {
-    out.push("PUT");
+    out.push(PUT);
   }
 
   if (posn.y < 0) {
-    out.push("NORTH");
+    out.push(NORTH);
   } else if (posn.y > 0) {
-    out.push("SOUTH");
+    out.push(SOUTH);
   } else {
-    out.push("PUT");
+    out.push(PUT);
   }
 
   return out;
+};
+
+/* Any -> Boolean
+  Return true if the value is a Direction
+ */
+module.exports.isDirection = function (val) {
+  return Array.isArray(val) &&
+    val.length === 2 &&
+    val[0] in [EAST, WEST, PUT] &&
+    val[1] in [NORTH, SOUTH, PUT];
 };
