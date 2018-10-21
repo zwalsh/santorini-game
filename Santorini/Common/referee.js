51d77e043d3d7acb517aa3a417979aa68bbbdce8
Suzanne Becker <suzanne@ccs.neu.edu>
2018-10-21 14:25:01 -0400

    Full implementation of Referee. 1. Data definitions for an in-progress or completed game. 2. Methods on the Referee to ensure that data (place, move, build requests) matches our data definitions. 3. Methods to set up, play, and play n games.

--- Santorini/Common/referee.js
+++ Santorini/Common/referee.js
@@ -1,5 +1,6 @@
 const Rulechecker = require('./rulechecker');
 const Board = require('../Common/board');
+const Direction = require('../Lib/direction');
 const c = require('../Lib/constants');
 
 /**
@@ -39,101 +40,196 @@ const c = require('../Lib/constants');
  *
  * An EndGameReason is one of:
  * - "WON"
- * - "BROKEN RULE"
+ * - "BROKEN_RULE"
+ * and represents whether a Player won legitimately, or because
+ * their opponent broke the rules.
  *
- * A GameResult is a [String, EndGameReason] where the String
- * is the name of the winner of the game.
+ * A GameResult is a [String, EndGameReason] where the string is
+ * the name of the Player who won the game.
  *
+ * A GameState is one of:
+ * - "IN_PROGRESS"
+ * - GameResult
+ * and represents a game being in-progress or over.
  *
  */
 
-
-/* Player Player -> GameResult
-  Manages a game of Santorini between the two given players.
-  Returns a GameResult representing the winner of the game, and the reason they won.
- */
-function playGame(player1, player2) {
-  // note: want a helper function that handles getting a turn from a player,
-  // format-checking and validating it, and maybe applying it to the board.
-}
-
-/* Player Player -> (Board | GameResult)
-  Takes two players through the setup state of a Santorini game:
-  placing workers on the board. Returns the complete initialized Board,
-  or if a player provides an invalid PlaceRequest, returns a GameResult
-  indicating that the other player won.
- */
-function setup(player1, player2) {
-
-}
-
-
-/* Player Player PositiveInteger -> [GameResult, ...]
-  Manages a game of Santorini between the two given players.
-  Returns a GameResult representing the winner of the game, and the reason they won.
- */
-function playNGames(player1, player2, numGames) {
-
-}
-
-module.exports = {
-  "playGame" : playGame,
-  "playNGames" : playNGames,
-}
-
-
-// Below: the class/design document that was part of the code we received.
-// We would like to proceed with a functional approach (above), but are leaving
-// it there for now in case that fails spectacularly.
+const RC = new Rulechecker();
 
 // A class to handle the different phases of the game - Initialization, Steady-State, and Game Over
 class Referee {
 
-  constructor() {
+  constructor(player1, player2) {
+    // Board holds the state of a game between the 2 players
     this.board = null;
-    this.playerMap = {};
+    // The 2 players that will play game(s) against each other
+    this.player1 = player1;
+    this.player2 = player2;
   }
 
-  // Called when a player connects to add player to the game
-  // String -> void
-  addPlayerToGame(name) {
-    // When a player connects, this creates a Player object and adds them to the player map
-    // Once MAX_PLAYERS are added, call initializeGame()
-  }
-
-  // After all players are connect, initialize the game board with ListOfInitWorkers
-  // and start the game
-  // void -> void
-  initializeGame() {
-    // Until max workers are added, take turns between players asking to submit a PlaceRequest
-    // If request is valid, turn it into an InitWorker and add to ListOfInitWorkers
-    // Initialize board and call playGame()
-  }
-
-  // Main Game loop, where we request actions from players and check their validity before applying them.
-  // Directly modifies the board stored by this class.
-  // void -> void
+  /* Void -> GameResult
+  Manages a game of Santorini between the two given players.
+  Notifies players of game start and end, and returns a GameResult
+  representing the winner of the game and the reason they won.
+ */
   playGame() {
-    // Requests a Turn from the Player whose turn it is by passing them a copy of the board
-    // Accepts request and sends to isValidTurnRequest()
-    // If valid request, apply Turn to the board, and update game phase
-    // If invalid request, restart loop and ask for another request
+    this.player1.newGame(this.player2.name);
+    this.player2.newGame(this.player1.name);
+
+    let gameState = this.setup();
+    let activePlayer = this.player1;
+    while (gameState === c.GameState.IN_PROGRESS) {
+      gameState = this.getAndApplyTurn(activePlayer);
+      activePlayer = this.flip(activePlayer);
+    }
+
+    this.player1.notifyGameOver(gameState.slice());
+    this.player2.notifyGameOver(gameState.slice());
+    return gameState;
   }
 
-  // Checks if the Turn given by a player is well-formed/valid.
-  // Turn -> Boolean
-  isValidTurnRequest(turn) { //  todo this is  definitely going into the rule checker -sb
-    // Parses out necessary data and dispatches to rulechecker
+  /* PositiveInteger -> [GameResult, ...]
+    Manages a given number of games of Santorini between the two given players.
+    Returns a list of GameResults representing the winner of the game, and the reason they won.
+    If a player breaks the rules in a game, that game is terminated and no further games are played.
+   */
+  playNGames(numGames) {
+    let gameResults = [];
+    for (let gameIdx = 0; gameIdx < numGames; gameIdx++) {
+      let result = this.playGame();
+      gameResults.push(result);
+      if (result[1] === c.EndGameReason.BROKEN_RULE) {
+        break;
+      }
+    }
+    return gameResults;
   }
 
-  // Checks if PlaceRequest given by a player is well-formed/valid
-  // PlaceRequest -> Boolean
-  isValidPlaceRequest(placeRequest) {
-    // Parses out necessary data and dispatches to rulechecker
+  /* Void -> GameState
+    Takes two players through the setup state of a Santorini game:
+    placing workers on the board. Initializes the board field on this Referee
+    with the completed Board, unless a player provides an invalid PlaceRequest.
+    In that case, returns a GameResult indicating that the other player won.
+   */
+  setup() {
+    let activePlayer = this.player1;
+    let initWorkerList = [];
+    let gameState = c.GameState.IN_PROGRESS;
+    while (gameState === c.GameState.IN_PROGRESS && initWorkerList.length < c.NUM_WORKERS) {
+      let placeReq = activePlayer.placeInitialWorker(Board.copyInitWorkerList(initWorkerList));
+
+      if (Referee.isWellFormedPlaceReq(placeReq) && RC.isValidPlace(initWorkerList, placeReq[1], placeReq[2])) {
+        let initWorker = {
+          player: activePlayer.name,
+          x: placeReq[1],
+          y: placeReq[2]
+        };
+        initWorkerList.push(initWorker);
+      } else {
+        return [this.flip(activePlayer).name, c.EndGameReason.BROKEN_RULE];
+      }
+      activePlayer = this.flip(activePlayer);
+    }
+    this.board = new Board(initWorkerList);
+    return gameState;
   }
 
-  // Actions to perform once the game is over
-  gameOver() {
+  /* Player -> GameState
+    Get the given Player's next Turn.
+    Apply the Turn to this Referee's Board if it:
+      - is well-formed
+      - refers to themself and not the other player
+      - and is valid according to the Rulechecker
+    Otherwise, return a GameResult indicating that the player broke the rules.
+   */
+  getAndApplyTurn(activePlayer) {
+    let turn = activePlayer.takeTurn(this.board.renderGame());
+    if (this.checkTurn(turn, activePlayer)) {
+      this.board.applyTurn(turn);
+    } else {
+      return [this.flip(activePlayer).name, c.EndGameReason.BROKEN_RULE];
+    }
 
+    if (RC.hasWon(this.board, activePlayer.name) || RC.hasLost(this.board, this.flip(activePlayer).name)) {
+      return [activePlayer.name, c.EndGameReason.WON];
+    } else {
+      return c.GameState.IN_PROGRESS;
+    }
+  }
+
+  /* Any Player -> Boolean
+    Return true if the input is a Turn that can be used by the Referee:
+    well-formed, refers to the right Player, and valid per the Rulechecker.
+   */
+  checkTurn(turn, activePlayer) {
+    return Referee.isWellFormedTurn(turn) &&
+      turn[1].player === activePlayer.name &&
+      RC.isValidTurn(this.board, turn);
+  }
+
+  /* Player -> Player
+    Switches the active player.
+   */
+  flip(activePlayer) {
+    return activePlayer === this.player1 ? this.player2 : this.player1;
+  }
+
+  /* Any -> Boolean
+    Return true if the input is a well-formed Turn.
+   */
+  static isWellFormedTurn(turn) {
+    return Array.isArray(turn) &&
+      turn.length >= 1 &&
+      Referee.isWellFormedMoveReq(turn[0]) &&
+      (turn.length === 1 ||
+        Referee.isWellFormedBuildReq(turn[1]));
+  }
+
+  /* Any -> Boolean
+    Return true if the input is a well-formed PlaceRequest.
+    PlaceRequest: ["place", x:Int, y:Int]
+   */
+  static isWellFormedPlaceReq(req) {
+    return Array.isArray(req) &&
+           req.length === 3 &&
+           req[0] === "place" &&
+           Number.isInteger(req[1]) &&
+           Number.isInteger(req[2]);
+  }
+
+  /* Any -> Boolean
+  Return true if the input is a well-formed MoveRequest.
+  MoveRequest: ["move", WorkerRequest, Direction]
+ */
+  static isWellFormedMoveReq(req) {
+    return Array.isArray(req) &&
+      req.length === 3 &&
+      req[0] === "move" &&
+      Referee.isWellFormedWorkerReq(req[1]) &&
+      Direction.isDirection(req[2]);
+  }
+
+  /* Any -> Boolean
+    Return true if the input is a well-formed MoveRequest.
+    BuildRequest: ["build", Direction]
+  */
+  static isWellFormedBuildReq(req) {
+    return Array.isArray(req) &&
+      req.length === 2 &&
+      req[0] === "build" &&
+      Direction.isDirection(req[1]);
+  }
+
+  /* Any -> Boolean
+    Return true if the input is a well-formed WorkerRequest.
+    WorkerRequest: { player:String, id:Int }
+   */
+  static isWellFormedWorkerReq(req) {
+    let keys = Object.keys(req);
+    return keys.length === 2 &&
+      keys.includes('player') &&
+      typeof req.player === 'string' &&
+      keys.includes('id') &&
+      Number.isInteger(req.id);
   }
 }
