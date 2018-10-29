3f6e15804efa2c8049042e1d3751b0f50c4f5266
Suzanne Becker <suzanne@ccs.neu.edu>
2018-10-29 10:36:24 -0400

    Observer executable for Assnmt 10

---
+++ Santorini/10/Aux/observe-complete-ai-game.js
@@ -0,0 +1,14 @@
+const Referee = require('../../Admin/referee');
+const Observer = require('../../Observer/observer');
+const Player = require('../../Player/player');
+const uuid = require('uuid/v4');
+
+let player1 = new Player();
+let player2 = new Player();
+let p1Id = 'Wayne';
+let p2Id = 'Garth';
+
+let referee = new Referee(player1, player2, p1Id, p2Id, 10);
+let observer = new Observer();
+referee.addObserver(observer);
+referee.playGame();
