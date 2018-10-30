ddb41edea1286266d9d3dde6210e9db39e26333c
Zach Walsh <zcwalsh@ccs.neu.edu>
2018-10-29 20:05:33 -0700

    Update the Referee to use GuardedPlayers only

--- Santorini/Admin/referee.js
+++ Santorini/Admin/referee.js
@@ -59,20 +59,15 @@ const RC = new Rulechecker();
 
 // A class to handle the different phases of the game - Initialization, Steady-State, and Game Over
 class Referee {
-  /* Player Player UUID UUID -> Referee
-    Construct a Referee to run game(s) between the given Players.
-    This Referee will use the given GUIDs to identify the Players as needed
-    internally, as well as in Board and Turn data it sends to other components.
+  /* GuardedPlayer GuardedPlayer -> Referee
+    Construct a Referee to run game(s) between the given players.
    */
-  constructor(player1, player2, p1Id, p2Id, timeout) {
+  constructor(player1, player2, timeout) {
     // Board holds the state of a game between the 2 players
     this.board = null;
     // The 2 players that will play game(s) against each other
-    this.player1 = new GuardedPlayer(player1, timeout);
-    this.player2 = new GuardedPlayer(player2, timeout);
-    // The UUID assigned to each player, respectively
-    this.p1Id = p1Id;
-    this.p2Id = p2Id;
+    this.player1 = player1;
+    this.player2 = player2;
     // List of Observers registered on this Referee.
     // Observers are trusted components that are given
     // references to essential game data (as opposed to copies).
@@ -86,12 +81,15 @@ class Referee {
   Cleanup: Resets referee board to null after the game is over.
  */
   playGame() {
-    let playersNotifiedOfStart = this.player1.newGame(this.p1Id, this.p2Id).then(() => {
-      return this.player2.newGame(this.p2Id, this.p1Id);
+    let p1Id = this.player1.getId();
+    let p2Id = this.player2.getId();
+
+    let playersNotifiedOfStart = this.player1.newGame(p1Id, p2Id).then(() => {
+      return this.player2.newGame(p1Id, p2Id);
     });
 
     let gameSetUp = playersNotifiedOfStart.then(() => {
-      this.notifyAllObservers(o => { o.startGame(this.p1Id, this.p2Id) });
+      this.notifyAllObservers(o => { o.startGame(p1Id, p2Id) });
       return this.setup();
     });
 
@@ -142,7 +140,7 @@ class Referee {
     The number of games in the series must be odd, or the behavior of this method is undefined.
    */
   playNGames(numGames) {
-    this.notifyAllObservers(o => { o.startSeries(this.p1Id, this.p2Id, numGames) });
+    this.notifyAllObservers(o => { o.startSeries(this.player1.getId(), this.player2.getId(), numGames) });
 
     return this.completePlayNGames(numGames, []).then((gameResults) => {
       this.notifyAllObservers(o => { o.seriesOver(gameResults) });
@@ -166,7 +164,7 @@ class Referee {
       } else {
         return gameResults;
       }
-    });//.catch(() => {});
+    });
   }
 
   /* [GameResult, ...] Natural -> GameResult
@@ -175,13 +173,13 @@ class Referee {
   */
   getSeriesStatus(gameResults, numGames) {
     let p1WinCount = gameResults
-      .filter(gameResult => (gameResult[0] === this.p1Id))
+      .filter(gameResult => (gameResult[0] === this.player1.getId()))
       .length;
     let p2WinCount = gameResults.length - p1WinCount;
     if (p1WinCount > numGames / 2) {
-      return [this.p1Id, c.EndGameReason.WON];
+      return [this.player1.getId(), c.EndGameReason.WON];
     } else if (p2WinCount > numGames / 2) {
-      return [this.p2Id, c.EndGameReason.WON];
+      return [this.player2.getId(), c.EndGameReason.WON];
     } else {
       return c.GameState.IN_PROGRESS
     }
@@ -209,15 +207,15 @@ class Referee {
     return activePlayer.placeInitialWorker(Board.copyInitWorkerList(initWorkerList)).then((placeReq) => {
       if (this.checkPlaceReq(placeReq, initWorkerList)) {
         let initWorker = {
-          player: this.playerId(activePlayer),
+          player: activePlayer.getId(),
           x: placeReq[1],
           y: placeReq[2]
         };
         initWorkerList.push(initWorker);
 
-        this.notifyAllObservers(o => { o.workerPlaced(placeReq, this.playerId(activePlayer), new Board(initWorkerList)) });
+        this.notifyAllObservers(o => { o.workerPlaced(placeReq, activePlayer.getId(), new Board(initWorkerList)) });
       } else {
-        return [this.playerId(this.flip(activePlayer)), c.EndGameReason.BROKEN_RULE];
+        return [this.flip(activePlayer).getId(), c.EndGameReason.BROKEN_RULE];
       }
 
       // Check for exit condition after worker is added to list of InitWorkers
@@ -228,7 +226,7 @@ class Referee {
         return this.completeSetup(this.flip(activePlayer), initWorkerList);
       }
     }).catch(() => {
-      return [this.playerId(this.flip(activePlayer)), c.EndGameReason.BROKEN_RULE];
+      return [this.flip(activePlayer).getId(), c.EndGameReason.BROKEN_RULE];
     });
   }
 
@@ -253,16 +251,16 @@ class Referee {
           o.turnTaken(turn, this.board)
         });
       } else {
-        return [this.playerId(this.flip(activePlayer)), c.EndGameReason.BROKEN_RULE];
+        return [this.flip(activePlayer).getId(), c.EndGameReason.BROKEN_RULE];
       }
 
-      if (RC.hasWon(this.board, this.playerId(activePlayer)) || RC.hasLost(this.board, this.playerId(this.flip(activePlayer)))) {
-        return [this.playerId(activePlayer), c.EndGameReason.WON];
+      if (RC.hasWon(this.board, activePlayer.getId()) || RC.hasLost(this.board, this.flip(activePlayer.getId()))) {
+        return [activePlayer.getId(), c.EndGameReason.WON];
       } else {
         return c.GameState.IN_PROGRESS;
       }
     }).catch(() => {
-      return [this.playerId(this.flip(activePlayer)), c.EndGameReason.BROKEN_RULE];
+      return [this.flip(activePlayer.getId()), c.EndGameReason.BROKEN_RULE];
     });
   }
 
@@ -284,7 +282,7 @@ class Referee {
    */
   checkTurn(turn, activePlayer) {
     return RFC.isWellFormedTurn(turn) &&
-      turn[0][1].player === this.playerId(activePlayer) &&
+      turn[0][1].player === activePlayer.getId() &&
       RC.isValidTurn(this.board, turn);
   }
 
@@ -293,13 +291,6 @@ class Referee {
    */
   flip(activePlayer) {
     return activePlayer === this.player1 ? this.player2 : this.player1;
-  }
-  
-  /* Player -> UUID
-    Get the UUID that this Referee has associated with the given Player.
-   */
-  playerId(activePlayer) {
-    return activePlayer === this.player1 ? this.p1Id : this.p2Id;
   }
 
   /* [Observer -> Void] -> Void
