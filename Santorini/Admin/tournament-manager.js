82ca406674d82bd30ce7f97f2c7cf9bfacf4250a
Suzanne Becker <suzanne@ccs.neu.edu>
2018-11-04 14:01:22 -0500

    TournamentManager implementation

--- Santorini/Admin/tournament-manager.js
+++ Santorini/Admin/tournament-manager.js
@@ -20,7 +20,11 @@
   GameResult data definition is in Admin/referee.js
 
  */
+const constants = require('../Common/constants');
 const MatchTable = require('./match-table');
+const TournamentResult = require('./tournament-result');
+const Referee = require('./referee');
+const TIMEOUT = 30000;
 
 class TournamentManager {
 
@@ -45,17 +49,19 @@ class TournamentManager {
     this.donePlayers = [];
 
     this.matchTable = new MatchTable(players.map(player => player.getId()));
-    this.tournamentResolutionPromise;
+    this.resolveTournament = null;
   }
 
   /* Void -> Promise<TournamentResult>
     Start the tournament, eventually promise to give back the results of the tournament.
    */
   startTournament() {
+    for (let player of this.players) {
+      this.matchOrWaitlistPlayer(player);
+    }
     return new Promise(resolve => {
-      this.tournamentResolutionPromise = resolve;
+      this.resolveTournament = resolve;
     });
-    // todo also kick off a series of matches
   }
 
   /* GP GP -> Void
@@ -63,26 +69,58 @@ class TournamentManager {
     and set the callback to handle the series result.
   */
   startMatch(player1, player2) {
-
+    let ref = this.createReferee(player1, player2);
+    return ref.playNGames(this.seriesLength).then((gameResults) => {
+      this.handleMatchResult(player1, player2, gameResults);
+    });
   }
 
-  /* [GameResult, ...] -> Void
+  /* GP GP -> Referee
+    Create a referee to run a match between the two players.
+  */
+  createReferee(player1, player2) {
+    return new Referee(player1, player2, TIMEOUT);
+  }
+
+  /* GP GP [GameResult, ...] -> Void
      Handles the result of a match between two players. This involves the following:
      - adding the match to the match table
      - checking if a player cheated during the match
      - matching the free players as necessary
      - check if the tournament is over and if so, end it
    */
-  handleMatchResult(gameResults) {
-
+  handleMatchResult(player1, player2, gameResults) {
+    this.matchTable.setMatch(player1.getId(), player2.getId(), gameResults);
+    let badPlayersInMatch = this.disqualifyBadPlayers(player1, player2);
+    this.badPlayers = this.badPlayers.concat(badPlayersInMatch);
+    [player1, player2].forEach((player) => {
+      if (!badPlayersInMatch.includes(player)) {
+        this.matchOrWaitlistPlayer(player);
+      }
+    });
+    if (this.isTournamentOver()) {
+      this.endTournament();
+    }
   }
 
-  /* GP GP -> Void
-    Checks if either of the two players cheated or broke in the match between the two of them.
-    If so, adds the offender(s) to this tournament's list of misbehaved players.
+  /* GP GP -> [GP, ...]
+    Checks if either of the two players cheated or broke in the match
+    between the two of them. If so, returns them in a list.
   */
-  removeBadPlayers(player1, player2) {
-
+  disqualifyBadPlayers(player1, player2) {
+    let match = this.matchTable.getMatch(player1.getId(), player2.getId());
+    if (match) {
+      if (match.length === 0) {
+        return [player1, player2]
+      }
+      let lastGameResult = match[match.length - 1];
+      if (lastGameResult.reason === constants.EndGameReason.BROKEN_RULE) {
+        let brokenPlayerName = lastGameResult.loser;
+        let brokenPlayer = brokenPlayerName === player1.getId() ? player1 : player2;
+        return [brokenPlayer];
+      }
+    }
+    return [];
   }
 
   /* GP -> Void
@@ -91,7 +129,30 @@ class TournamentManager {
     empty), then place them on the waitlist too.
   */
   matchOrWaitlistPlayer(player) {
+    let remainingOpponentNames = this.matchTable.getRemainingOpponents(player.getId());
+    if (remainingOpponentNames.length === 0) {
+      this.donePlayers.push(player);
+      return;
+    }
+    let maybeOpponent = this.waitingPlayers.find(player => {
+      return remainingOpponentNames.includes(player.getId());
+    });
+    if (maybeOpponent) {
+      this.removeFromWaitlist(maybeOpponent);
+      this.startMatch(player, maybeOpponent);
+    } else {
+      this.waitingPlayers.push(player);
+    }
+  }
 
+  /* GP -> Void
+    Remove the given player from the waiting list.
+  */
+  removeFromWaitlist(player) {
+    let newWaitingPlayers = this.waitingPlayers.filter(waitingPlayer => {
+      return player !== waitingPlayer;
+    });
+    this.waitingPlayers = newWaitingPlayers;
   }
 
   /* Void -> Boolean
@@ -99,7 +160,16 @@ class TournamentManager {
     either in the done list or the cheater list.
   */
   isTournamentOver() {
+    return this.players.length === (this.donePlayers.length + this.badPlayers.length);
+  }
 
+  /* Void -> Void
+    Calls the stored resolution function from the start of the tournament
+    with the tournament's current results.
+  */
+  endTournament() {
+    let tournamentResult = new TournamentResult(this.players, this.badPlayers, this.matchTable);
+    this.resolveTournament(tournamentResult);
   }
 }
 
