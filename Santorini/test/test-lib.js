6a8545e9b50dc46923305cd6abbd42837c3838e2
Suzanne Becker <suzanne@ccs.neu.edu>
2018-11-16 19:47:44 -0500

    Add test library function for deep copy assertion

--- Santorini/test/test-lib.js
+++ Santorini/test/test-lib.js
@@ -1,4 +1,6 @@
 const sinon = require('sinon');
+const chai = require('chai');
+const assert = chai.assert;
 
 /* String, ... -> Object
   Mocks an object by taking in a variable number of method names to mock,
@@ -10,4 +12,16 @@ module.exports.createMockObject = function createMockObject() {
     object[method] = sinon.stub();
   }
   return object;
+}
+
+/* [X, ...] [X, ...] -> Void
+  Assert that the lists are deep copies of each other
+  i.e. each pair of elements are deeply equal but not strictly equal (!==)
+*/
+module.exports.assertDeepCopy = function(arr1, arr2) {
+  assert.equal(arr1.length, arr2.length);
+  for (let i = 0; i < arr1.length; i++) {
+    assert.deepEqual(arr1[i], arr2[i]);
+    assert.notStrictEqual(arr1[i], arr2[i]);
+  }
 }
