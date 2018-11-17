19be748efc04d117335c153a585857aa1e299723
Zach Walsh <zcwalsh@ccs.neu.edu>
2018-11-17 13:04:59 -0800

    Move (a copy of) the JSON parsing function to Lib/

---
+++ Santorini/Lib/json-parser.js
@@ -0,0 +1,16 @@
+const nearley = require('nearley');
+const grammar = require('./jsongrammar');
+
+// Robust string-to-JSON parser, supporting multiple JSON inputs in nearly any format.
+// If multiple JSON objects or arrays are submitted in one input, they are in order in the output list.
+// String -> ListOfJSON
+exports.jsonParser = function (input) {
+  let parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+  parser.feed(input.trim());
+  if (parser.results.length !== 0) {
+    let res = parser.results;
+    return res[0];
+  } else {
+    return [];
+  }
+};
