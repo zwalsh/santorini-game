1899eb70f2f6dcc56c1726bf5fd1aa82178584b5
Zach Walsh <zcwalsh@ccs.neu.edu>
2018-11-20 08:20:12 -0800

    Implement client message conversion to/from JSON

--- Santorini/Remote/client-message-converter.js
+++ Santorini/Remote/client-message-converter.js
@@ -6,37 +6,47 @@
   ========== Data Definitions ==========
 
   Internal:
-  - Board
-  - Worker
-  - Turn
-  - InitWorker
-  - WorkerRequest
-  - GameResult
-
+  - Board is defined in Common/board.js
+  - Worker is defined in Common/worker.js
+  - Turn is defined in Common/player-interface.js
+  - InitWorker is defined in Common/board.js
+  - GameResult is defined in Common/game-result.js
 
   JSON:
-  - JsonWorker is (Worker) ...
-  - Cell is ...
-  - Placement
-  - WorkerPlace is ...
-  - Coordinate is ...
-  - Action is ...
-  - EncounterOutcome is ...
-
- */
+  - A Cell is defined in RuleCheckerHarness.js
+  - A Placement is an array: [WorkerPlace, ...] of up to length 3, representing
+    a list of already-placed Workers.
+  - A WorkerPlace is a JSON array: [JsonWorker,Coordinate,Coordinate].
+  - A JsonWorker is a String of the player's name followed by either
+    a 1 or a 2, denoting which of the player's Workers it is.
+  - A Coordinate is a natural number between 0 and 5 (inclusive).
+  - An Action is either:
+      - String, which represents the name of a player that is giving up;
+      - [Worker,EastWest,NorthSouth], which represents a winning move; or
+      - [Worker,EastWest,NorthSouth,EastWest,NorthSouth], which represents
+         a request to move the specified worker and build in the specified
+         directions. The first pair of directions specify the move, the
+         second one the building step.
+  - An EncounterOutcome is one of the following:
+      - [String, String], which is the name of the winner followed by
+        the loser;
+      - [String, String, "irregular"], which is like the first
+        alternative but signals that the losing player misbehaved.
+*/
 
 const Worker = require('../Common/worker');
 const Board = require('../Common/board');
+const GameResult = require('../Common/game-result');
 const constants = require('../Common/constants');
 
 
 // ================= X to JSON ==================
 
 /* PlaceRequest -> Place
-
- */
+  Converts a PlaceRequest into the equivalent Place message.
+*/
 function placeRequestToJson(placeRequest) {
-
+  return [placeRequest[1], placeRequest[2]];
 }
 
 /* Turn -> Action
@@ -56,11 +66,21 @@ function turnToJson(turn) {
 // ================= JSON to X ==================
 
 /* Placement -> [InitWorker, ...]
-
+  Convert the received Placement into an InitWorkerList, with each
+  WorkerPlace in the list converted to the equivalent InitWorker.
 */
-function jsonToInitWorkerList(initWorkerList) {
-
+function jsonToInitWorkerList(placement) {
+  return placement.map(jsonToInitWorker);
 }
+
+/* WorkerPlace -> InitWorker
+  Convert the given WorkerPlace into the equivalent InitWorker.
+*/
+function jsonToInitWorker(workerPlace) {
+  let workerRequest = jsonToWorkerRequest(workerPlace[0]);
+  return { player: workerRequest.player, x: workerPlace[1], y: workerPlace[2] };
+}
+
 
 /* [[Cell, ...] ...] -> Board
   Create a Board object from the given JSON representation.
@@ -107,10 +127,30 @@ function jsonToBoard(boardSpec) {
 }
 
 /* [EncounterOutcome, ...] -> [GameResult, ...]
-
+  Convert the list of EncounterOutcomes to an equivalent list of GameResults.
 */
 function jsonToGameResults(encounterOutcomes) {
+  return encounterOutcomes.map(jsonToGameResult);
+}
 
+/* EncounterOutcome -> GameResult
+  Convert the given EncounterOutcome to the equivalent GameResult.
+*/
+function jsonToGameResult(encounterOutcome) {
+  let endGameReason = encounterOutcome.length === 2 ? constants.EndGameReason.WON : constants.EndGameReason.BROKEN_RULE;
+  return new GameResult(encounterOutcome[0], encounterOutcome[1], endGameReason);
+}
+
+
+/* Worker -> WorkerRequest
+  Convert the string worker representation to a WorkerRequest
+ */
+function jsonToWorkerRequest(worker) {
+  let parsedWorker = worker.split(/[0-9]/);
+  let player = parsedWorker[0];
+  let id = parseInt(worker.substring(worker.length - 1));
+
+  return {player: player, id: id};
 }
 
 
