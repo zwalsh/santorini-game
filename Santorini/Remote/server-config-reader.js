dca37c3379cbc1ee034c69c51367c557b5a05348
Suzanne Becker <suzanne@ccs.neu.edu>
2018-11-21 12:00:13 -0500

    Create and test server configuration reader

---
+++ Santorini/Remote/server-config-reader.js
@@ -0,0 +1,92 @@
+/*
+  Accepts a Santorini server configuration as a string,
+  and creates a SantoriniServer from the configuration.
+
+  A configuration will be a string containing a JSON object with
+  the following form:
+
+    {
+      "min players" : Natural,
+
+      "port"        : [50000, 60000],
+
+      "waiting for" : Natural,
+
+      "repeat"      : {0|1}
+    }
+
+  "waiting for" specifies the minimum number of seconds to wait for before
+  starting a tournament.
+
+  "min players" specifies the minimum number of player connections to
+  wait for before starting a tournament.
+
+  "repeat" is 0 if the server will shut down after one complete tournament,
+  or 1 if it will run an arbitrary number of tournaments (closing
+  player connections after each tournament).
+
+ */
+
+const parseJson = require('../Lib/json-parser').jsonParser;
+
+/* String -> [Maybe SantoriniServer]
+  Creates a SantoriniServer from the configuration specified in the
+  string, if the config is valid.
+*/
+function createServer(configStr) {
+  let maybeConfig = parseConfig(configStr);
+  if (maybeConfig) {
+    let minPlayers = maybeConfig["min players"];
+    let port = maybeConfig["port"];
+    let waitingFor = maybeConfig["waiting for"];
+    let repeat = maybeConfig["repeat"];
+    return new SantoriniServer(minPlayers, port, waitingFor, repeat);
+  }
+  return false;
+}
+
+/* String -> [Maybe Configuration]
+  Parse the string to a Configuration object and verify that the
+  Configuration is well-formed before returning it.
+ */
+function parseConfig(configStr) {
+  let parsedJson;
+  try {
+    parsedJson = parseJson(configStr);
+  } catch (err) {
+    return false;
+  }
+  if (parsedJson.length !== 1) {
+    return false;
+  }
+  let config = parsedJson[0];
+  let keys = Object.keys(config);
+  if (keys.length !== 4) {
+    return false;
+  }
+  let minPlayers = config["min players"];
+  let port = config["port"];
+  let waitingFor = config["waiting for"];
+  let repeat = config["repeat"];
+
+  if (!(Number.isInteger(minPlayers) && minPlayers > 0)) {
+    return false;
+  }
+  if (!(Number.isInteger(port) && port >= 50000 && port <= 60000)) {
+    return false;
+  }
+  if (!(Number.isInteger(waitingFor) && waitingFor > 0)) {
+    return false;
+  }
+  if (!(Number.isInteger(repeat) && (repeat === 0 || repeat === 1))) {
+    return false;
+  }
+  return config;
+}
+
+module.exports = {
+  'createServer': createServer
+};
+
+
+
---
+++ Santorini/test/server-config-reader-test.js
@@ -0,0 +1,108 @@
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+
+let createServer = require('../Remote/server-config-reader').createServer;
+
+function makeConfigObj(minPlayers, port, waitingFor, repeat) {
+  return {
+    "min players" : minPlayers,
+    "port"        : port,
+    "waiting for" : waitingFor,
+    "repeat"      : repeat
+  };
+}
+
+describe('ServerConfigReader tests', function () {
+  describe('createServer', function () {
+    describe('when given an invalid configuration string', function () {
+      describe('that is not JSON', function () {
+        it('returns false', function () {
+          let configStr = '[not, json';
+          let result = createServer(configStr);
+          assert.isFalse(result);
+        });
+      });
+
+      describe('that is an object with too many keys', function () {
+        let configStr;
+        beforeEach(function () {
+          let configObj = makeConfigObj(2, 50000, 10, 1);
+          configObj.bad = "bad";
+          configStr = JSON.stringify(configObj);
+        });
+        it('returns false', function () {
+          let result = createServer(configStr);
+          assert.isFalse(result);
+        });
+      });
+
+      describe('that is missing a required key', function () {
+        let configStr;
+        beforeEach(function () {
+          let configObj = makeConfigObj(2, 50000, 10, 1);
+          delete configObj.repeat;
+          configStr = JSON.stringify(configObj);
+        });
+        it('returns false', function () {
+          let result = createServer(configStr);
+          assert.isFalse(result);
+        });
+      });
+
+      describe('where the values do not match the specification', function () {
+        describe('min players', function () {
+          let configStr;
+          beforeEach(function () {
+            let configObj = makeConfigObj({}, 50000, 10, 1);
+            configStr = JSON.stringify(configObj);
+          });
+          it('returns false', function () {
+            let result = createServer(configStr);
+            assert.isFalse(result);
+          });
+        });
+
+        describe('port', function () {
+          let configStr;
+          beforeEach(function () {
+            let configObj = makeConfigObj(2, 420, 10, 1);
+            configStr = JSON.stringify(configObj);
+          });
+          it('returns false', function () {
+            let result = createServer(configStr);
+            assert.isFalse(result);
+          });
+        });
+
+        describe('waiting for', function () {
+          let configStr;
+          beforeEach(function () {
+            let configObj = makeConfigObj(2, 50000, -1, 1);
+            configStr = JSON.stringify(configObj);
+          });
+          it('returns false', function () {
+            let result = createServer(configStr);
+            assert.isFalse(result);
+          });
+        });
+
+        describe('repeat', function () {
+          let configStr;
+          beforeEach(function () {
+            let configObj = makeConfigObj(2, 50000, 10, 'hello');
+            configStr = JSON.stringify(configObj);
+          });
+          it('returns false', function () {
+            let result = createServer(configStr);
+            assert.isFalse(result);
+          });
+        });
+      });
+    });
+
+    describe('when given a valid configuration string', function () {
+      it('returns a SantoriniServer');
+    });
+  });
+});
