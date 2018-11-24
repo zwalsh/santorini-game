50fa253caff745545d64102ade49b97ec88a9c4b
Zach Walsh <zcwalsh@ccs.neu.edu>
2018-11-24 08:43:03 -0800

    Add server skeleton plus incomplete unit tests

--- Santorini/Remote/server.js
+++ Santorini/Remote/server.js
@@ -11,29 +11,43 @@
   =================== Data Definitions ===================
 */
 
+const net = require('net');
+
 class TournamentServer {
 
-  /* Natural Natural Natural Natural -> TournamentServer
+  /* Natural Natural Natural Boolean -> TournamentServer
 
     Create a TournamentServer with the given configuration options.
       minPlayers : The minimum number of players to wait for
       port       : The port to listen on, in the range [50000, 60000]
-      waitingFor : The minimum number of seconds to wait for
-      repeat     : 0 to run only one tournament, 1 to run indefinitely
+      waitingFor : The maximum number of milliseconds to wait for
+      repeat     : whether the server should repeat
   */
-  constructor(minPlayers, port, waitingFor, repeat) {
+  constructor(minPlayers, port, waitingFor, repeat, host) {
     this.minPlayers = minPlayers;
     this.port = port;
+    this.host = host;
     this.waitingFor = waitingFor;
+    this.waitingForTimeout;
     this.repeat = repeat;
     this.sockets = [];
     this.server = this.createServer();
   }
 
   /* Void -> Void
-    Start listening on the port.
+    Start listening on the port. Starts a timer that will
+    shut down the server when it has waited for the specified
+    amount of time.
   */
   start() {
+    // this.server.listen(host, port);
+    // this.waitingForTimeout = this.createWaitingForTimeout();
+  }
+
+  /* Void -> Timeout
+    Creates the timeout that calls shutdown after waitingFor seconds.
+  */
+  createTimeout() {
 
   }
 
@@ -41,17 +55,71 @@ class TournamentServer {
     Create the networked server that can accept client connections.
    */
   createServer() {
+    return new net.Server(this.handleConnection);
+  }
+
+  /* Socket -> Void
+    Add the incoming socket connection to the list.
+    If enough sockets have been received, start the tournament,
+    and destroy any more that are received.
+  */
+  handleConnection(socket) {
+    // can receive more socket calls???
+    if (this.sockets.length >= this.minPlayers) {
+      socket.destroy();
+      return;
+    }
+  }
+
+  /* Void -> Void
+    Create a TournamentManager with the players behind the network
+    connections. Run the tournament. Shutdown or reset the
+    server state when the tournament is over.
+  */
+  createAndRunTournament() {
+    // wrap sockets: GP(RPP(PJS(socket)))
+    // clear the timeout
+    // tm.run().then(shutdown);
+  }
+
+  /* Void -> Void
+    Closes all socket connections and closes the server if only one
+    tournament is to be played.
+  */
+  shutdown() {
+    // close all connections
+    // clear everything out of the socket list
+    // clear the timeout
+    // if(!repeat)
+    // server.close()
+  }
+
+  /* PromiseJsonSocket -> Promise<GuardedPlayer>
+    Creates a GuardedPlayer out of a PromiseJsonSocket.
+  */
+  createPlayerWithSocket(pjs) {
+    // use promise protector here
+    return this.getPlayerName(pjs).then((name) => {
+      new GP(RPP(), null, timeout);
+    });
+  }
+
+  /* [GuardedPlayer, ...] -> Promise<TournamentManager>
+    Creates a TournamentManager that manages a tournament
+    between the given list of GuardedPlayers. Ensures
+    that all of their names are unique, and only
+    creates the TM if at least two players are properly set up.
+  */
+  createTournamentManager(players) {
 
   }
 
-  /* Additional methods
-    - callback for handling incoming socket
-      - call server.close() after the last socket
-    - callback for server closing (if repeat==true, reopen)
-    - method to close all socket connections
-
-   */
-
+  /* PromiseJsonSocket -> Promise<String>
+    Gets the player's name out of the PromiseJsonSocket.
+  */
+  getPlayerName(socket) {
+    // use promise protector here
+  }
 }
 
 module.exports = TournamentServer;
--- Santorini/test/server-tests.js
+++ Santorini/test/server-tests.js
@@ -1,5 +1,160 @@
 const chai = require('chai');
+const sinon = require('sinon');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 const expect = chai.expect;
 const assert = chai.assert;
+const testLib = require('./test-lib');
 
 const TournamentServer = require('../Remote/server');
+
+describe('TournamentServer', function () {
+  let ts, minPlayers, port, waitingFor, repeat, host;
+  beforeEach(function () {
+    minPlayers = 2;
+    port = 50000;
+    waitingFor = 1000;
+    repeat = 0;
+    host = '127.0.0.1';
+  });
+  describe('start', function () {
+    let timeoutObj;
+    beforeEach(function () {
+      timeoutObj = {};
+      ts = new TournamentServer(minPlayers, port, waitingFor, repeat);
+      ts.server = testLib.createMockObject('listen');
+      ts.createTimeout = sinon.stub();
+      ts.createTimeout.returns(timeoutObj);
+      ts.start();
+    });
+    it('starts the server listening on the port', function () {
+      assert.isTrue(ts.server.listen.calledWith(port, host));
+    });
+    it('creates the waitingFor timeout and sets it on the object', function () {
+      assert.isTrue(ts.createTimeout.called);
+      assert.equal(ts.waitingForTimeout, timeoutObj);
+    });
+  });
+  describe('createTimeout', function () {
+    beforeEach(function () {
+      waitingFor = 10;
+      ts = new TournamentServer(minPlayers, port, waitingFor, repeat, host);
+      ts.shutdown = sinon.stub();
+      ts.createTimeout();
+    });
+    it('creates a timeout that calls shutdown after waitingFor seconds', function () {
+      return new Promise(() => {
+        return setTimeout(() => {
+          return assert.isTrue(ts.shutdown.calledOnce);
+        }, waitingFor + 1);
+      });
+    });
+  });
+  describe('handleConnection', function () {
+    let socket;
+    beforeEach(function () {
+      ts = new TournamentServer(minPlayers, port, waitingFor, repeat);
+      ts.createAndRunTournament = sinon.stub();
+      socket = testLib.createMockObject();
+    });
+    describe('when more than one player is needed to start a tournament', function () {
+      beforeEach(function () {
+        ts.handleConnection(socket);
+      });
+      it('adds the socket to the list', function () {
+        assert.deepEqual(ts.sockets, [socket]);
+      });
+      it('does not start the tournament', function () {
+        assert.isFalse(ts.createAndRunTournament.called);
+      });
+    });
+    describe('when one more player is needed to start a tournament', function () {
+      beforeEach(function () {
+        ts.sockets = [{}];
+        ts.handleConnection(socket);
+      });
+      it('adds the socket to the list', function () {
+        assert.deepEqual(ts.sockets, [{}, socket]);
+      });
+      it('starts the tournament', function () {
+        assert.isTrue(ts.createAndRunTournament.calledOnce);
+      });
+    });
+    describe('when the server already has enough connections', function () {
+      beforeEach(function () {
+        ts.sockets = [{}, {}];
+        socket = testLib.createMockObject('destroy');
+        ts.handleConnection(socket);
+      });
+      it('destroys the incoming connection', function () {
+        assert.isTrue(socket.destroy.calledOnce);
+      });
+      it('does not add the connection to the list', function () {
+        assert.deepEqual(ts.sockets, [{}, {}]);
+      });
+      it('does not start the tournament', function () {
+        assert.isFalse(ts.createAndRunTournament.isCalled);
+      });
+    });
+  });
+  describe('createAndRunTournament', function () {
+    beforeEach(function () {
+      ts = new TournamentServer(minPlayers, port, waitingFor, repeat);
+    });
+    describe('when less than two players are correctly set up', function () {
+      beforeEach(function () {
+        ts.createPlayerWithSocket = sinon.stub.rejects();
+        ts.createAndRunTournament();
+      });
+      it('does not start the tournament manager');
+    });
+    describe('when a player cannot be set up from the socket', function () {
+      it('excludes that player from the tournament');
+    });
+    it('makes the TournamentManager');
+    it('wraps all sockets in RemoteProxyPlayers and GuardedPlayers');
+    it('ensures all names are unique');
+    it('clears the waitingForTimeout');
+  });
+  describe('createTournamentManager', function () {
+    beforeEach(function () {
+      // will need to mock player objects w getId, setId
+    });
+    describe('when not enough players accept unique names', function () {
+      // getId should return same id, setId should reject
+      it('does not create the TournamentManager', function () {
+
+      });
+    });
+    describe('when enough players have unique names', function () {
+      // getId should return same id
+      it('creates the TournamentManager with those players', function () {
+
+      });
+    });
+  });
+  describe('shutdown', function () {
+    it('closes all sockets');
+    it('removes all sockets from the list');
+    describe('when the tournament is supposed to repeat', function () {
+      it('does not shut down the server');
+    });
+    describe('when the tournament is not supposed to repeat', function () {
+      it('shuts down the server');
+    });
+  });
+  describe('createPlayerWithSocket', function () {
+    it('gets the player\'s name', function () {
+
+    });
+    it('returns a GuardedPlayer with a RemoteProxyPlayer');
+  });
+  describe('getPlayerName', function () {
+    describe('when the PJS provides a name', function () {
+      it('resolves with the name');
+    });
+    describe('when the PJS does not provide a name', function () {
+      it('rejects');
+    });
+  });
+});
