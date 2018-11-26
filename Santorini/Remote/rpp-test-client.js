9c608c707f0d5363e93659bc7bd9d749b5e2e5db
Zach Walsh <zcwalsh@ccs.neu.edu>
2018-11-26 15:54:07 -0800

    End the process in the test client when the socket closes

--- Santorini/Remote/rpp-test-client.js
+++ Santorini/Remote/rpp-test-client.js
@@ -1,11 +1,15 @@
 const net = require('net');
 
-let socket = net.createConnection(54321, '127.0.0.1');
+let socket = net.createConnection(50000, '127.0.0.1');
 
 socket.on('data', function (data) {
   if (data) {
     process.stdout.write(data + '\n');
   }
+});
+
+socket.on('end', function () {
+  process.exit(0);
 });
 
 process.stdin.on('data', function (data) {
